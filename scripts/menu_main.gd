28131f7ab5ebb8cb8fd1c92b5021bd1b581e58d6
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-08-24 13:39:25 -0500

    add game

---
+++ scripts/menu_main.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_button_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/intro_fall.tscn")
+	pass # Replace with function body.s
+
+
+func _on_button_settings_pressed() -> void:
+	pass # Replace with function body.
---
+++ scripts/player.gd
@@ -0,0 +1,37 @@
+extends RigidBody3D
+
+@onready var camera = $TwistPivot/PitchPivot/Camera3D
+
+var deadzone = 0.1
+
+# ty https://www.youtube.com/watch?v=sVsn9NqpVhg
+
+# runs when node enters scene tree for the first time
+func _ready() -> void:
+	pass
+
+# called every frame, delta is elapsed time since prev. frame
+# delta allows variations between machines
+func _process(delta: float) -> void:
+	# var input = Input.get_action_strength("ui_up")
+	# apply_central_force(input * Vector3.FORWARD * 1200.0 * delta)
+	
+	var input := Vector3.ZERO
+	
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.z = Input.get_axis("ui_up", "ui_down")
+	
+	# map keyboard controls to relatively move based on camera (45deg/cardinal movement only)
+	var camera_basis = camera.global_transform.basis
+	
+	var vertical = camera_basis.z
+	var horizontal = camera_basis.x
+	
+	# unused/ignored axis
+	vertical.y = 0
+	horizontal.y = 0
+	
+	# works with both keyboard and joystick controls 🔥
+	var dir = (vertical * input.z + horizontal * input.x).normalized()
+	
+	apply_central_force(dir * 1200.0 * delta)
