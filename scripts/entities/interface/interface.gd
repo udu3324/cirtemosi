b62c78c1359beeed16a4aa04f0300578bd933471
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-09-27 01:00:29 -0500

    fix and finish interface animations + wip dialogue ui

--- scripts/entities/interface/interface.gd
+++ scripts/entities/interface/interface.gd
@@ -6,12 +6,23 @@ extends Node3D
 @onready var screen_tex: NoiseTexture2D = screen.material.albedo_texture
 @onready var audio_player = $Node3D/AudioStreamPlayer3D
 
+@onready var text: Node3D = $Node3D/Text
+@onready var underscore: Node3D = $Node3D/Text/Underscore
+
+@onready var panel = $SubViewport/InterfaceDialogue
+@onready var intro = $SubViewport/InterfaceDialogue/MarginContainer2
+@onready var dialogue = $SubViewport/InterfaceDialogue/MarginContainer
+
+var terminal_color: Color = Color(0.021, 0.258, 0.021, 1.0)
+
 var player_is_close: bool = false
 var player_is_interacting: bool = false
 
 var flashing: bool = false
 
 var noise_period: float = 0.0
+var turn_period: float = 0.0
+var underscore_period: float = 0.0
 
 var model_rotation_rest
 var model_position_rest
@@ -31,11 +42,53 @@ func _process(delta: float) -> void:
 		screen_tex.noise.seed += 1
 		noise_period = 0
 	
+	
+	if text.visible:
+		panel.visible = true
+		_handle_input()
+	else:
+		panel.visible = false
+	
+	
+	
 	if player_is_interacting:
-		screen_material.albedo_color = Color(0.0, 0.039, 1.0, 1.0)
-		screen_material.emission = Color(0.0, 0.039, 1.0, 1.0)
-		screen_material.emission_enabled = true
+		underscore_period += delta
+		if underscore_period > 1.0:
+			underscore.visible = !underscore.visible
+			
+			underscore_period = 0
+		
+		turn_period += delta
+		if turn_period > 6.0:
+			tween = create_tween()
+			_face_to_vector3(Globals.player_pos)
+			
+			turn_period = 0
+		
+		if !flashing:
+			flashing = true
+			
+			await _flicker()
+			if randf() < 0.5:
+				await get_tree().create_timer(randf_range(1.0, 1.5)).timeout
+				await _flicker()
+			
+			await get_tree().create_timer(randi_range(6, 9)).timeout
+			
+			flashing = false
+		
+		if !text.visible:
+			await get_tree().create_timer(2.0).timeout
+			
+			# stops user from going back and forth glitching the text
+			if !player_is_interacting:
+				return
+			
+			text.visible = true
+		
 		return
+	else:
+		text.visible = false
 	
 	# animation pass for far
 	if player_is_close and !flashing:
@@ -50,17 +103,36 @@ func _process(delta: float) -> void:
 		
 		flashing = false
 
+func _handle_input():
+	if Input.is_action_just_pressed("attack"):
+		if intro.visible:
+			Globals.player_can_move = false
+			intro.visible = false
+			dialogue.visible = true
+		else:
+			Globals.player_can_move = true
+			intro.visible = true
+			dialogue.visible = false
 
 func _flicker():
+	screen_material.emission_energy_multiplier = randf_range(0.3, 0.8)
+	
 	screen_material.albedo_color = Color(0.0, 0.0, 0.0, 0.0)
 	screen_material.emission = Color(0.0, 0.0, 0.0, 0.0)
 	screen_material.emission_enabled = true
 	
-	await get_tree().create_timer(randf_range(0.1, 0.5)).timeout
-	
-	screen_material.albedo_color = Color(0.0, 0.0, 0.0, 1.0)
-	screen_material.emission = Color(0.0, 0.0, 0.0, 1.0)
-	screen_material.emission_enabled = false
+	if player_is_interacting:
+		await get_tree().create_timer(0.1).timeout
+		
+		screen_material.albedo_color = terminal_color
+		screen_material.emission = terminal_color
+	else:
+		await get_tree().create_timer(randf_range(0.1, 0.5)).timeout
+		
+		screen_material.albedo_color = Color(0.0, 0.0, 0.0, 1.0)
+		screen_material.emission = Color(0.0, 0.0, 0.0, 1.0)
+		screen_material.emission_enabled = false
+		
 
 func _on_outer_area_3d_body_entered(body: Node3D) -> void:
 	if body.name.contains("Player"):
@@ -70,11 +142,14 @@ func _on_outer_area_3d_body_exited(body: Node3D) -> void:
 	if body.name.contains("Player"):
 		player_is_close = false
 
-var tween
+var tween = create_tween()
 
 func _on_inner_area_3d_body_entered(body: Node3D) -> void:
 	if body.name.contains("Player"):
 		player_is_interacting = true
+		
+		if tween.is_running():
+			tween.kill()
 		
 		tween = create_tween()
 		tween.parallel().tween_property(model, "rotation:x", -deg_to_rad(13.5), 1.3).as_relative()
@@ -84,19 +159,38 @@ func _on_inner_area_3d_body_entered(body: Node3D) -> void:
 		tween.parallel().tween_property(model, "position:y", 1, 1.5).as_relative()
 		
 		_face_to_vector3(Globals.player_pos)
+		
+		screen_material.albedo_color = terminal_color
+		screen_material.emission = terminal_color
+		screen_material.emission_enabled = true
+		screen_material.emission_energy_multiplier = 0.4
+		
+		screen.material.emission_texture = null
+		screen.material.albedo_texture = null
 
 func _on_inner_area_3d_body_exited(body: Node3D) -> void:
 	if body.name.contains("Player"):
 		player_is_interacting = false
 		
+		if tween.is_running():
+			tween.kill()
+		
 		tween = create_tween()
 		
-		tween.parallel().tween_property(model, "global_position", model_position_rest, 1.0)
+		tween.parallel().tween_property(model, "global_position", model_position_rest, 0.3)
 
 		tween.parallel().tween_property(model, "rotation:x", model_rotation_rest.x, 0.3)
 		tween.parallel().tween_property(model, "rotation:z", model_rotation_rest.z, 0.3)
 		
-
+		screen_material.albedo_color = Color(0.0, 0.0, 0.0, 1.0)
+		screen_material.emission = Color(0.0, 0.0, 0.0, 1.0)
+		screen_material.emission_enabled = false
+		flashing = false
+		
+		screen.material.emission_texture = preload("res://assets/gdtex/screen_noise.tres")
+		screen.material.albedo_texture = preload("res://assets/gdtex/screen_noise.tres")
+		
+		text.visible = false
 
 
 
