eefb3e959df38777bacc9d9a90e7659ed54c1076
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-10-12 23:57:29 -0500

    add a sign for hints

---
+++ scripts/enviornment/sign.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@export var text: String = "placeholder"
+
+@onready var sign_label: Label = $SubViewport/CenterContainer/Label
+@onready var control: HBoxContainer = $SubViewport/CenterContainer/HBoxContainer
+
+func _ready() -> void:
+	sign_label.text = text
+
+func _process(_delta: float) -> void:
+	if !control.visible:
+		return
+	
+	if Input.is_action_just_pressed("attack"):
+		control.visible = false
+		sign_label.visible = true
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name.contains("Player"):
+		control.visible = true
+		sign_label.visible = false
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.name.contains("Player"):
+		control.visible = false
+		sign_label.visible = false
--- scripts/items/bow.gd
+++ scripts/items/bow.gd
@@ -8,6 +8,7 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		print_debug("picking up bow")
 		self.visible = false
 		Globals.equipment[1] = "bow"
+		Globals.slot_active = 2
 		
 		audio_player.play()
 		
