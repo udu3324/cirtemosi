14c6f22d712d9aa39c6585e543d705b656f6b38d
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-10-14 22:58:35 -0500

    fix relic ui having missing stuff

--- scripts/gui/relics.gd
+++ scripts/gui/relics.gd
@@ -2,13 +2,13 @@ extends Control
 
 @onready var relic_1_silhouette = $MarginContainer/HBoxContainer/CenterContainer
 @onready var relic_1 = $MarginContainer/HBoxContainer/Relic1
-@onready var relic_2_silhouette = $MarginContainer/HBoxContainer/CenterContainer
+@onready var relic_2_silhouette = $MarginContainer/HBoxContainer/CenterContainer2
 @onready var relic_2 = $MarginContainer/HBoxContainer/Relic2
-@onready var relic_3_silhouette = $MarginContainer/HBoxContainer/CenterContainer
+@onready var relic_3_silhouette = $MarginContainer/HBoxContainer/CenterContainer3
 @onready var relic_3 = $MarginContainer/HBoxContainer/Relic3
-@onready var relic_4_silhouette = $MarginContainer/HBoxContainer/CenterContainer
+@onready var relic_4_silhouette = $MarginContainer/HBoxContainer/CenterContainer4
 @onready var relic_4 = $MarginContainer/HBoxContainer/Relic4
-@onready var relic_5_silhouette = $MarginContainer/HBoxContainer/CenterContainer
+@onready var relic_5_silhouette = $MarginContainer/HBoxContainer/CenterContainer5
 @onready var relic_5 = $MarginContainer/HBoxContainer/Relic5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
